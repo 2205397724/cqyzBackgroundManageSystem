43683358e634cfca7abf15f7ecebec69389ae035
小宇 <1097455706@qq.com>
2022-01-20 18:00:22 +0800

    房屋页 统计静态

---
+++ src/components/PositionTree/index.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="diytree">
+        <div class="tit">
+            {{props.data.tit}}
+        </div>
+        <div class="tree-box">
+            <el-tree
+                highlight-current
+                :data="props.data.data"
+                :props="{ children: 'children', label: 'label' }"
+                node-key="id"
+                accordion
+                :default-expanded-keys="props.data.default_expanded"
+                @node-click="handleNodeClick" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import {reactive, defineProps, defineEmits} from 'vue'
+    const props = defineProps(['data']);
+    const emit = defineEmits(['myClick','nullClick'])
+    const handleNodeClick = (item,e) => {
+        if(!item.children||item.children.length<=0){
+            const breadCrumb = [];
+            const getFName = (e)=>{
+                if(e.level > 0){
+                    breadCrumb.unshift(e.data.label)
+                    getFName(e.parent)
+                }
+            }
+            getFName(e)
+            emit('myClick', {breadCrumb:breadCrumb,item:item})
+        }else{
+            emit('nullClick', '')
+        }
+    }
+</script>
+<style lang="scss">
+    .diytree{
+        height: 100%;
+        overflow: auto;
+        .tit{
+            color: #999999;
+            font-size: 13px;
+            height: 60px;
+            line-height: 60px;
+            box-sizing: border-box;
+            padding-left: 20px;
+            border-right: 1px solid rgba(233, 233, 233, 1);
+            border-bottom: 1px solid rgba(233, 233, 233, 1);
+        }
+        .tree-box{
+            height: calc(100% - 60px);
+            box-sizing: border-box;
+            border-right: 1px solid rgba(233, 233, 233, 1);
+            border-bottom: 1px solid rgba(233, 233, 233, 1);
+            padding-bottom: 20px;
+            .el-tree-node__label{
+             font-size: 13px;
+            }
+        }
+    }
+</style>
--- src/views/statistics/house.vue
+++ src/views/statistics/house.vue
@@ -1,19 +1,149 @@
 <template>
     <div>
-        <page-header title="欢迎使用 Fantastic-admin（基础版）">
-            <template #content>
-                <div>
-                    <div style="margin-bottom: 5px;">这是一款<b class="text-emphasis">开箱即用</b>的中后台框架，同时它也经历过数十个真实项目的技术沉淀，确保框架在开发中可落地、可使用、可维护</div>
-                    <div>注：在作者就职的公司，本框架已在电商、直播、OA、ERP等多个不同领域的中后台系统中应用并稳定运行</div>
+        <page-main style="padding: 0;">
+            <div class="tree-box">
+                <div class="tree-item">
+                    <position-tree :data="place_list" @myClick="placeClick" @nullClick="placeNullClick"></position-tree>
                 </div>
-            </template>
-        </page-header>
+                <div class="tree-item">
+                    <position-tree :data="house_list" @myClick="houseClick" @nullClick="houseNullClick"></position-tree>
+                </div>
+                <div class="tree-details">
+                    <div class="tit">
+                        <div style="min-width: 80px;">你选择的是：</div>
+                        <div class="breadCrumb" v-if="(BreadCrumb.house&&BreadCrumb.house.length>0)&&(BreadCrumb.place&&BreadCrumb.place.length>0)">
+                            <span v-for="(item,i) in BreadCrumb.place" :key="'BreadCrumbPlace'+i">{{item}}</span>
+                            <span v-for="(item,i) in BreadCrumb.house" :key="'BreadCrumbPlace'+i">{{item}}</span>
+                        </div>
+                        <div class="breadCrumb" v-else>
+                            <span>左边选择“区域”“小区”！</span>
+                        </div>
+                    </div>
+                    <div class="num-box">
+                        <div class="num-box-item" v-for="i in 2">
+                            <div class="box-tit">数量统计：</div>
+                            <div class="flexs">
+                                <div class="flex-item" v-for="j in 2">
+                                    <div class="item-num">26</div>
+                                    <div class="item-tit">小区总数</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </page-main>
     </div>
 </template>
+<script setup>
+    import {
+        reactive
+    } from 'vue'
+    const place_list = reactive({
+        tit: '选择区域',
+        default_expanded: ['50', '5001'],
+        data: [{
+                id: '50',
+                label: '重庆市',
+                children: [{
+                    id: '5001',
+                    label: '万州区',
+                }, ],
+            }
+        ]
+    })
+    const house_list = reactive({
+        tit: '选择小区',
+        default_expanded: ['50', '5001'],
+        data: [
+            {
+                id: '30',
+                label: '安徽省',
+            }
+        ]
+    })
+    const BreadCrumb = reactive({place:'',house:''})
+    const clickData = reactive({place:'',house:''})
+    const placeClick = (data)=>{
+        BreadCrumb.place = data.breadCrumb
+        clickData.place = data.item
+        placeNullClick()
+        house_list.data = [{
+                id: '30',
+                label: '安徽省',
+            }]
+    }
+    const houseClick = (data)=>{
+        BreadCrumb.house = data.breadCrumb
+        clickData.house = data.item
+    }
+    const placeNullClick = (data)=>{
+        house_list.data = []
+        houseNullClick()
+    }
+    const houseNullClick = (data)=>{
 
+    }
+
+</script>
 <style lang="scss" scoped>
-.text-emphasis {
-    /* stylelint-disable-next-line property-no-vendor-prefix */
-    -webkit-text-emphasis-style: "❤";
-}
+    .tree-box{
+        display: flex;
+        .tree-item{
+            min-width: 200px;
+            width: 200px;
+        }
+        .tree-details{
+            flex-grow: 1;
+            .tit{
+                height: 60px;
+                box-sizing: border-box;
+                border-bottom: 1px solid rgba(233, 233, 233, 1);
+                font-size: 13px;
+                color: #999999;
+                padding-left: 20px;
+                padding-right: 20px;
+                box-sizing: border-box;
+                display: flex;
+                justify-content: left;
+                align-items: center;
+                span{
+                    color: #333333;
+                    font-size: 14px;
+                }
+                span::after{
+                    content: '>';
+                    display: inline-block;
+                    margin: 0 6px;
+                }
+                span:last-child::after{
+                    display: none;
+                }
+            }
+
+            .num-box{
+                padding: 20px;
+                box-sizing: border-box;
+                .num-box-item{
+                    .box-tit{
+                        border-bottom: 1px solid rgba(233, 233, 233, 1);
+                        box-sizing: border-box;
+                    }
+                    .flexs{
+                        .flex-item{
+                            .item-num{
+
+                            }
+                            .item-tit{
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    .num-box{
+
+    }
 </style>
