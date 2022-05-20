0deeb86c3a6d52f1ce7fec0e8146e0918996d8b9
âasdabc <13996221127@163.com>
2022-05-20 10:30:55 +0800

    查看发起的流程和详情

--- src/views/event/events.vue
+++ src/views/event/events.vue
@@ -49,35 +49,8 @@
                         <span>{{ scope.row.updated_at }} </span>
                     </template>
                 </el-table-column>
-                <el-table-column fixed="right" label="操作" width="400">
+                <el-table-column fixed="right" label="操作" width="210">
                     <template #default="scope">
-                        <el-button
-                            type="success" size="small"
-                            @click="flowworkFuncEventSaveFunc(scope.row)"
-                        >
-                            保存
-                        </el-button>
-                        <el-popconfirm
-                            title="确定要提交当前项么?"
-                            cancel-button-type="info"
-                            @confirm="clickFuncUpdate(scope.row)"
-                        >
-                            <template #reference>
-                                <el-button
-                                    type="primary"
-                                    size="small"
-                                >
-                                    提交
-                                </el-button>
-                            </template>
-                        </el-popconfirm>
-                        <el-button
-                            size="small"
-                            type="warning"
-                            @click="flowevent.switch_back=true;flowwork.active = scope.row;getOptBZListFunc(scope.row)"
-                        >
-                            返回
-                        </el-button>
                         <el-button
                             size="small"
                             @click="detailsFlowworkFunc(scope.row)"
@@ -208,6 +181,16 @@
                         <div class="left">更新时间</div>
                         <div class="right">{{ flowwork.details.updated_at }}</div>
                     </div>
+                    <div class="item">
+                        <div class="left">流程模板名称</div>
+                        <div class="right">{{ flowwork.details.flow.name }}</div>
+                    </div>
+                    <div class="item">
+                        <div class="left">流程步骤</div>
+                        <div class="right">
+                            <div v-for="(item,i) in flowwork.details.flow.steps" :style="{'color':flowwork.details.step==item.step?'#67c23A':''}">{{item.name}}</div>
+                        </div>
+                    </div>
                 </div>
                 <template #footer>
                     <span class="dialog-footer">
@@ -277,135 +260,60 @@
                             <span>{{ scope.row.updated_at }} </span>
                         </template>
                     </el-table-column>
-
+                    <el-table-column fixed="right" label="操作" width="110">
+                        <template #default="scope">
+                            <el-button
+                                size="small"
+                                @click="showShiXiang(scope.row)"
+                            >
+                                详情
+                            </el-button>
+                        </template>
+                    </el-table-column>
                     <el-table-column />
                 </el-table>
             </el-dialog>
-            <!-- 保存 -->
-            <el-dialog
-                v-model="flowevent.switch_save"
-                title="保存当前事项进度"
-                width="50%"
-            >
-                <el-form
-                    ref="ruleFormRef"
-                    :model="flowevent.form_save"
-                >
-                    <el-row :gutter="10">
-                        <el-col :xs="24" :sm="24" :md="24" :lg="12" :xl="12">
-                            <el-form-item
-                                label-width="70px"
-                                label="议题"
-                                :error="flowevent.error_save&&flowevent.error_save.topictype?flowevent.error_save.topictype[0]:''"
-                            >
-                                <el-select v-model="flowevent.form_save.topictype" class="head-btn" placeholder="" clearable>
-                                    <el-option v-for="(item,i) in opts_all.obj.step_yt_type" :key="item.key" :label="item.val" :value="item.key" />
-                                </el-select>
-                            </el-form-item>
-                        </el-col>
-                        <el-col :xs="24" :sm="24" :md="24" :lg="12" :xl="12">
-                            <el-form-item
-                                label-width="70px"
-                                label="议题对象"
-                                :error="flowevent.error_save&&flowevent.error_save.forval?flowevent.error_save.forval[0]:''"
-                            >
-                                <CascaderType v-model="flowevent.form_save.forval" />
-                            </el-form-item>
-                        </el-col>
-                        <el-col :xs="24" :sm="24" :md="24" :lg="12" :xl="12">
-                            <el-form-item
-                                label-width="70px"
-                                label="对象ID"
-                                :error="flowevent.error_save&&flowevent.error_save.topicid?flowevent.error_save.topicid[0]:''"
-                            >
-                                <!-- 行政端为 区域代码、企业端和业主端为 小区ID -->
-                                <!-- <div style="height: 100%;box-sizing: border-box;padding-bottom: 10px;width: 100%;">
-                                <div style="box-sizing: border-box;border-radius: 4px;border: 1px solid #dcdfe6;width: 100%;height: 100%;font-size: 14px;">
-                                    <SearchResidential v-model:str="flowevent.form_save.topicid" />
-                                </div>
-                            </div> -->
-                                <Cascaders v-model="flowevent.form_save.topicid" />
-                            </el-form-item>
-                        </el-col>
-                        <el-col :md="24" :lg="24">
-                            <div style="margin-bottom: 10px;">
-                                <el-button type="primary" plain @click="addServiceFuncSave">添加自定义字段</el-button>
-                            </div>
-                            <div v-for="(item,i) in flowevent.form_save.extra" class="serve-box">
-                                <el-row :gutter="10">
-                                    <el-col :xs="12" :sm="12">
-                                        <el-form-item label="自定义字段名" :error="flowevent.error_save&&flowevent.error_save[`extra.${i}.label`]?flowevent.error_save[`extra.${i}.label`][0]:''">
-                                            <el-input
-                                                v-model="item.label"
-                                                placeholder=""
-                                            />
-                                        </el-form-item>
-                                    </el-col>
-                                    <el-col :xs="12" :sm="12">
-                                        <el-form-item label="自定义值" :error="flowevent.error_save&&flowevent.error_save[`extra.${i}.type`]?flowevent.error_save[`extra.${i}.type`][0]:''">
-                                            <el-input
-                                                v-model="item.val"
-                                                placeholder=""
-                                            />
-                                        </el-form-item>
-                                    </el-col>
-                                    <el-col :xs="12" :sm="12">
-                                        <el-form-item label="自定义类型" :error="flowevent.error_save&&flowevent.error_save[`extra.${i}.type`]?flowevent.error_save[`extra.${i}.type`][0]:''">
-                                            <el-input
-                                                v-model="item.type"
-                                                placeholder=""
-                                            />
-                                        </el-form-item>
-                                    </el-col>
-                                </el-row>
-                                <div class="delete-service" @click="deleteServiceFuncSave(i)">
-                                    <el-icon :size="20" color="#F56C6C">
-                                        <el-icon-circle-close />
-                                    </el-icon>
-                                </div>
-                            </div>
-                        </el-col>
-                    </el-row>
-                </el-form>
-                <template #footer>
-                    <div style="display: flex;justify-content: flex-end;align-items: center;width: 100%;">
-                        <el-button @click="flowevent.switch_save=false">取消</el-button>
-                        <el-button type="primary" @click="flowworkFuncEventSaveFuncUpdata">确定</el-button>
-                    </div>
-                </template>
-            </el-dialog>
-            <!-- 保存 -->
-            <el-dialog
-                v-model="flowevent.switch_back"
-                title="返回事件进度"
-                width="50%"
-            >
-                <el-form
-                    ref="ruleFormRef"
-                    :model="flowevent.form_back"
-                >
-                    <el-row :gutter="10">
-                        <el-col :xs="24" :sm="24" :md="24" :lg="12" :xl="12">
-                            <el-form-item
-                                label-width="70px"
-                                label="步骤节点"
-                                :error="flowevent.error_back&&flowevent.error_back.topictype?flowevent.error_back.topictype[0]:''"
-                            >
-                                <el-select v-model="flowevent.form_back.step" class="head-btn" placeholder="" clearable>
-                                    <el-option v-for="(item,i) in opt_bz.obj" :key="item.key" :label="item.name" :value="item.step" />
-                                </el-select>
-                            </el-form-item>
-                        </el-col>
-                    </el-row>
-                </el-form>
-                <template #footer>
-                    <div style="display: flex;justify-content: flex-end;align-items: center;width: 100%;">
-                        <el-button @click="flowevent.switch_back=false">取消</el-button>
-                        <el-button type="primary" @click="flowworkFuncEventSaveFuncEvent">确定</el-button>
-                    </div>
-                </template>
-            </el-dialog>
         </page-main>
+        <!-- 事项详情 -->
+        <el-dialog
+            v-model="shixiang_details.switch_details"
+            title="详情"
+            width="50%"
+        >
+            <div class="details-box">
+                <div class="item">
+                    <div class="left">步骤代码</div>
+                    <div class="right">{{ shixiang_details.details.step }}</div>
+                </div>
+                <div class="item">
+                    <div class="left">终端类型</div>
+                    <div class="right">{{ getOptVal(opts_all.obj.terminal,shixiang_details.details.doutype) }}</div>
+                </div>
+                <div class="item">
+                    <div class="left">创建时间</div>
+                    <div class="right">{{ shixiang_details.details.created_at }}</div>
+                </div>
+                <div class="item">
+                    <div class="left">更新时间</div>
+                    <div class="right">{{ shixiang_details.details.updated_at }}</div>
+                </div>
+                <div class="item">
+                    <div class="left">自定义表单值</div>
+                    <div class="right">
+                        <div style="display: flex;" v-for="(item,i) in shixiang_details.details.extra">
+                            <div>{{item.label}}：</div>
+                            <div v-if="item.type=='checkbox'||item.type=='picker-select'">{{getDiyOptVal(item.options,item.val)}}</div>
+                            <div v-else>{{ item.val }}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button @click="shixiang_details.switch_details = false">取消</el-button>
+                </span>
+            </template>
+        </el-dialog>
     </div>
 </template>
 <script setup>
@@ -486,9 +394,9 @@ const addAndModifyFlowworkFunc = () => {
 
 import {
     APIgetFlowworkEventList,
-    APIpostFlowworkEventSave,
-    APIpostFlowworkEventUpdate,
-    APIpostFlowworkEventSpeed
+    // APIpostFlowworkEventSave,
+    // APIpostFlowworkEventUpdate,
+    // APIpostFlowworkEventSpeed
 } from '@/api/custom/custom.js'
 const flowevent = reactive({
     switch_list: false,
@@ -508,63 +416,17 @@ const flowworkFuncEventList = val => {
         flowevent.switch_list = true
     })
 }
-const flowworkFuncEventSaveFunc = val => {
-    flowwork.active = val
-    flowevent.error_save = {}
-    flowevent.form_save = {}
-    flowevent.switch_save = true
-}
-const flowworkFuncEventSaveFuncUpdata = val => {
-    flowevent.error_save = {}
-    APIpostFlowworkEventSave(flowwork.active.id, flowevent.form_save).then(res => {
-        flowworkFuncEventList(flowwork.active.id)
-        flowevent.switch_save = false
-    }).catch(err => {
-        flowevent.error_save = err.data
-    })
-}
-// 添加字段
-const addServiceFuncSave = index => {
-    let data = {
-        'label': '',
-        'type': '',
-        'val': ''
-    }
-    if (!flowevent.form_save.extra) {
-        flowevent.form_save.extra = []
-    }
-    flowevent.form_save.extra.push(data)
-}
-// 删除
-const deleteServiceFuncSave = index => {
-    flowevent.form_save.extra.splice(index, 1)
-}
-const clickFuncUpdate = val => {
-    flowwork.active = val
-    APIpostFlowworkEventUpdate(flowwork.active.id).then(res => {
-        flowworkFuncEventList(flowwork.active.id)
-    })
-}
-const flowworkFuncEventSaveFuncEvent = val => {
-    APIpostFlowworkEventSpeed(flowwork.active.id, flowevent.form_back).then(res => {
-        flowworkFuncEventList(flowwork.active.id)
-        flowevent.switch_back = false
-    }).catch(err => {
-        flowevent.error_back = err.data
-    })
+const shixiang_details = reactive({
+    switch_details: false,
+    details: ''
+})
+const showShiXiang = val=>{
+    console.log(val)
+    shixiang_details.details = val;
+    shixiang_details.switch_details = true;
 }
 /* ----------------------------------------------------------------------------------------------------------------------- */
-import {
-    APIgetStepList
-} from '@/api/custom/custom.js'
-const opt_bz = reactive({
-    obj: []
-})
-const getOptBZListFunc = val => {
-    APIgetStepList(val.fid).then(res => {
-        opt_bz.obj = res.data
-    })
-}
+
 /* ----------------------------------------------------------------------------------------------------------------------- */
 // 刷新
 const flowworkRefreshFunc = () => {
@@ -580,10 +442,28 @@ import { getOpts, getOptVal } from '@/util/opts.js'
 const opts_all = reactive({
     obj: {}
 })
-getOpts(['terminal_num', 'type_type', 'flow_active', 'article_lv', 'step_yt_type', 'step_back', 'status_all']).then(res => {
+getOpts(['terminal_num', 'type_type', 'flow_active', 'article_lv', 'step_yt_type', 'step_back', 'status_all','terminal']).then(res => {
     opts_all.obj = res
 })
-
+const getDiyOptVal = (arr,val)=>{
+    if(Array.isArray(val)){
+        let str = ''
+        for(let i in val){
+            for(let j in arr){
+                if(arr[j].value == val[i]){
+                    str+=','+arr[j].text
+                }
+            }
+        }
+        return str.substring(1)
+    }
+    for(let i in arr){
+        if(arr[i].value==val){
+            return arr[i].text
+        }
+    }
+    return ''
+}
 </script>
 <style lang="scss">
     .setupgroup {
