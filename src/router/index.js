1070bc72dda022b76ad0327b0b40325c59c0d4a2
WIN-HQCBF0RM27G\Administrator <2205397724@qq.com>
2022-10-21 09:58:37 +0800

    合并的数据

--- src/router/index.js
+++ src/router/index.js
@@ -287,68 +287,57 @@ router.beforeEach(async(to, from, next) => {
     settingsOutsideStore.app.enableProgress && (isLoading.value = true)
     // 是否已登录
     if (userOutsideStore.isLogin) {
-        if(userOutsideStore.isChooseCity){
-            // 是否已根据权限动态生成并挂载路由
-            if (menuOutsideStore.isGenerate) {
-                // 导航栏如果不是 single 模式，则需要根据 path 定位主导航的选中状态
-                settingsOutsideStore.menu.menuMode !== 'single' && menuOutsideStore.setHeaderActived(to.path)
-                if (to.name) {
-                    if (to.matched.length !== 0) {
-                        // 如果已登录状态下，进入登录页会强制跳转到控制台页面
-                        if (to.name == 'login') {
+        // 是否已根据权限动态生成并挂载路由
+        if (menuOutsideStore.isGenerate) {
+            // 导航栏如果不是 single 模式，则需要根据 path 定位主导航的选中状态
+            settingsOutsideStore.menu.menuMode !== 'single' && menuOutsideStore.setHeaderActived(to.path)
+            if (to.name) {
+                if (to.matched.length !== 0) {
+                    // 如果已登录状态下，进入登录页会强制跳转到控制台页面
+                    if (to.name == 'login') {
+                        next({
+                            name: 'dashboard',
+                            replace: true
+                        })
+                    } else if (!settingsOutsideStore.dashboard.enable && to.name == 'dashboard') {
+                        // 如果未开启控制台页面，则默认进入侧边栏导航第一个模块
+                        if (menuOutsideStore.sidebarRoutes.length > 0) {
                             next({
-                                name: 'dashboard',
+                                path: menuOutsideStore.sidebarRoutesFirstDeepestPath,
                                 replace: true
                             })
-                        } else if (!settingsOutsideStore.dashboard.enable && to.name == 'dashboard') {
-                            // 如果未开启控制台页面，则默认进入侧边栏导航第一个模块
-                            if (menuOutsideStore.sidebarRoutes.length > 0) {
-                                next({
-                                    path: menuOutsideStore.sidebarRoutesFirstDeepestPath,
-                                    replace: true
-                                })
-                            } else {
-                                next()
-                            }
                         } else {
                             next()
                         }
                     } else {
-                        // 如果是通过 name 跳转，并且 name 对应的路由没有权限时，需要做这步处理，手动指向到 404 页面
-                        next({
-                            path: '/404'
-                        })
+                        next()
                     }
                 } else {
-                    next()
+                    // 如果是通过 name 跳转，并且 name 对应的路由没有权限时，需要做这步处理，手动指向到 404 页面
+                    next({
+                        path: '/404'
+                    })
                 }
             } else {
-                let accessRoutes = []
-                if (!settingsOutsideStore.app.enableBackendReturnRoute) {
-                    accessRoutes = await menuOutsideStore.generateRoutesAtFront(asyncRoutes)
-                } else {
-                    accessRoutes = await menuOutsideStore.generateRoutesAtBack()
-                }
-                accessRoutes.push(lastRoute)
-                let removeRoutes = []
-                accessRoutes.forEach(route => {
-                    if (!/^(https?:|mailto:|tel:)/.test(route.path)) {
-                        removeRoutes.push(router.addRoute(route))
-                    }
-                })
-                // 记录的 accessRoutes 路由数据，在登出时会使用到，不使用 router.removeRoute 是考虑配置的路由可能不一定有设置 name ，则通过调用 router.addRoute() 返回的回调进行删除
-                menuOutsideStore.setCurrentRemoveRoutes(removeRoutes)
-                next({ ...to, replace: true })
-            }
-        }else{
-            if(to.path =='/city'){
                 next()
-            }else{
-                next({
-                    path: '/city',
-                    replace: true
-                })
             }
+        } else {
+            let accessRoutes = []
+            if (!settingsOutsideStore.app.enableBackendReturnRoute) {
+                accessRoutes = await menuOutsideStore.generateRoutesAtFront(asyncRoutes)
+            } else {
+                accessRoutes = await menuOutsideStore.generateRoutesAtBack()
+            }
+            accessRoutes.push(lastRoute)
+            let removeRoutes = []
+            accessRoutes.forEach(route => {
+                if (!/^(https?:|mailto:|tel:)/.test(route.path)) {
+                    removeRoutes.push(router.addRoute(route))
+                }
+            })
+            // 记录的 accessRoutes 路由数据，在登出时会使用到，不使用 router.removeRoute 是考虑配置的路由可能不一定有设置 name ，则通过调用 router.addRoute() 返回的回调进行删除
+            menuOutsideStore.setCurrentRemoveRoutes(removeRoutes)
+            next({ ...to, replace: true })
         }
     } else {
         if (to.name != 'login') {
--- src/views/city.vue
+++ src/views/city.vue
@@ -2,60 +2,28 @@
     <div>
         <page-main>
             选择城市
-            <div class="citylist">
-                <div v-for="(item,index) in cityList.arr" :key="index" :class="{item:true,select:(choose.city.china_code==item.china_code)}" @click="click_city(item)">
-                   {{ item.name }}
-                </div>
-            </div>
         </page-main>
     </div>
 </template>
-<script setup name="city">
-import { APIgetCityNotPm } from '@/api/custom/custom.js'
-const { proxy } = getCurrentInstance()
-import { useSettingsStore } from '@/store/modules/settings'
-import { ElMessage } from 'element-plus'
-//已选择
-const choose = reactive({
-    city : {}
-})
-//城市列表
-const cityList = reactive({
-    arr: []
-})
-// 获取城市配置
-const getCityList = () => {
-    let params = {
-        page: 1,
-        per_page: 500
-    }
-    APIgetCityNotPm(params).then(res => {
-        let uid = localStorage.getItem('uid')
-        choose.city = JSON.parse(localStorage.getItem(uid + '_user_city')) 
-        // res.forEach((item, index) => {
-        //     if (item.china_code == city.china_code) {
-        //         item.flag = true
-        //     } else {
-        //         item.flag = false
-        //     }
-        // })
-        cityList.arr = res
-    })
-}
-// 选择 后确认按钮
-const click_city = val => {
-    if (!val.china_code) {
-        ElMessage.error('请选择城市')
-        return
-    }
-    let uid = localStorage.getItem('uid')
-    localStorage.setItem(uid + '_user_city', JSON.stringify({ china_code: val.china_code, name: val.name }))
-    ElMessage.success('选择成功')
-}
-onMounted(() => {
-    getCityList()
-})
-</script>
-<style lang="scss" scoped>
+<script setup>
 
+</script>
+
+<style lang="scss" scoped>
+    @media only screen and (min-width: 1700px) {
+        .el-col-xl-4-8 {
+            max-width: 20%;
+        }
+        .el-col-xl-offset-4-8 {
+            margin-left: 20%;
+        }
+        .el-col-xl-pull-4-8 {
+            position: relative;
+            right: 20%;
+        }
+        .el-col-xl-push-4-8 {
+            position: relative;
+            left: 20%;
+        }
+    }
 </style>
